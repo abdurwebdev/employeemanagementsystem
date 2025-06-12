28d46d6d9f4746337ffd0d6ce4114e5e7c421f47
abdurwebdev <iabdurrehman22.0@gmail.com>
2025-06-12 20:45:32 +0500

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-cente font-[gilroy] min-h-screen bg-gray-100">
+      <div className="p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+        <p className="mt-2 text-gray-600">This is a basic React component styled with Tailwind CSS.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
